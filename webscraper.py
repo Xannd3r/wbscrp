abd1d0c9c30085e171850a958c7ee4763b95d288
Ammar <39661674+Xannd3r@users.noreply.github.com>
2023-10-03 22:17:31 -0600

    Add files via upload

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,24 @@
 import requests
+from bs4  import BeautifulSoup
+
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
+
+#need to save the scraped file to the folder here
+
+
+
+#open the scraped file here
+with open("your_html_file.html", "r", encoding="utf-8") as html_file:
+    soup = BeautifulSoup(page.content, "html.parser")
+
+#save as a file
+with open("parsed_data.txt", "w", encoding="utf-8") as output_file:
+    title = soup.title.text
+output_file.write("Title: " + title + "\n")
+
+
+
 
 print(page.text)
